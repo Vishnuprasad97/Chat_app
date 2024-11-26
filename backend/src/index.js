4ff71b4f6b71f948c3a28813b37179bc7166fe4b
Vishnuprasad97 <vishnuprasad9391@gmail.com>
2024-11-26 19:50:30 +0530

    started front end

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,24 @@
 // importing required files
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 //defining app
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser);
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 //test
 const PORT = process.env.PORT;
